ad35d9a7cef136a1817b53bf2c8b00deec3a142b
Thomas <thomas.huisman@outlook.com>
2026-06-15 16:58:47 +0200

    feat: display version in UI

--- reso/web/vite.config.ts
+++ reso/web/vite.config.ts
@@ -1,21 +1,29 @@
 import react from '@vitejs/plugin-react';
+import { readFileSync } from 'fs';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
+const { version } = JSON.parse(readFileSync('./package.json', 'utf-8')) as {
+  version: string;
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': '/src',
-		},
-	},
-	plugins: [
-		react(),
-		svgr({
-			include: '**/*.svg?react',
-			svgrOptions: {
-				// plugins: ['@svgr/plugin-svgo']
-			},
-		}),
-	],
+  define: {
+    __RESO_VERSION__: JSON.stringify(version),
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg?react',
+      svgrOptions: {
+        // plugins: ['@svgr/plugin-svgo']
+      },
+    }),
+  ],
 });
