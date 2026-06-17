2fe8e1eade4b27baa841acb69d1d14a70aa6c5fb
Thomas <thomas.huisman@outlook.com>
2026-06-17 22:22:39 +0200

    add domain_count column drop to migration

--- reso/migrations/10-subscription-cache-headers/up.sql
+++ reso/migrations/10-subscription-cache-headers/up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE list_subscriptions DROP COLUMN hash;
 ALTER TABLE list_subscriptions ADD COLUMN etag TEXT;
 ALTER TABLE list_subscriptions ADD COLUMN last_modified TEXT;
+ALTER TABLE list_subscriptions DROP COLUMN domain_count;
 CREATE INDEX IF NOT EXISTS idx_domain_rules_subscription_id ON domain_rules (subscription_id);
