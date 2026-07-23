1c556fd422324fe144d80d11d2d8289100870618
Thomas <thomas.huisman@outlook.com>
2026-07-24 00:31:45 +0200

    fix: typo

--- reso/web/src/app/pages/home.tsx
+++ reso/web/src/app/pages/home.tsx
@@ -117,7 +117,7 @@ export default function HomePage() {
 					isLoading={timelineLoading}
 				/>
 				<StatCard
-					label='Average Response time'
+					label='Average Response Time'
 					value={`${avgResponse} ms`}
 					icon={Clock}
 					accentColor='accent'
